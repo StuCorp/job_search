fcb9227cc3c45bd0f70c1522555ecaebddf6fdd1
Stuart Bryce <stuartlbryce@gmail.com>
2022-07-14 16:01:51 +0100

    button , image components added

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -24,14 +24,24 @@
             </li>
           </ul>
         </nav>
+        <div class="flex items-center h-full ml-auto">
+          <profile-image v-if="isLoggedIn" />
+          <action-button v-else />
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from "@/components/ActionButton.vue";
+import ProfileImage from "@/components/ProfileImage.vue";
 export default {
   name: "MainNav",
+  components: {
+    ActionButton,
+    ProfileImage,
+  },
   data() {
     return {
       company: "Eggman Careers",
@@ -44,6 +54,7 @@ export default {
         "Students",
         "Jobs",
       ],
+      isLoggedIn: false,
     };
   },
 };
---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="imageLink" class="w-8 h-8 object-contain rounded-3xl" />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageLink:
+        "https://pm1.narvii.com/6213/598fb5e9254000ea1653e0fa8057a65cffbd5c54_128.jpg",
+    };
+  },
+};
+</script>
